85cbad7a8d6b7df6a74062b4778c3757076e6db4
Rasel Sheikh <raselsheikh123@gmail.com>
2025-02-12 08:52:33 +0600

    mysql connect and json encoded

--- config.php
+++ config.php
@@ -2,20 +2,18 @@
 $conn = mysqli_connect("localhost", "root", "", "json_practice") or die('Connection Error');
 $sql = "SELECT * FROM `student_info`";
 $result = mysqli_query($conn, $sql) or die('Query Error');
-/*$data_arr = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$data_arr = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+/*
+$data_arr = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $data_arr[] = $row;
+
+}*/
 echo "<pre>";
 echo json_encode($data_arr);
-echo "</pre>";*/
-
-$output = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $output[] = $row;
-
-}
-echo "<pre>";
-echo json_encode($output);
 echo "</pre>";
+
 
 
 
