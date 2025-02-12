f8639f419361745957f35f06df44de416170f39e
Rasel Sheikh <raselsheikh123@gmail.com>
2025-02-12 08:50:02 +0600

    init

--- config.php
+++ config.php
@@ -1,30 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CRUD</title>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <div id="wrapper">
-        <div id="header">
-            <h1>Crud</h1>
-        </div>
-        <div id="menu">
-            <ul>
-                <li>
-                    <a href="index.php">Home</a>
-                </li>
-                <li>
-                    <a href="add.php">Add</a>
-                </li>
-                <li>
-                    <a href="update.php">Update</a>
-                </li>
-                <li>
-                    <a href="delete.php">Delete</a>
-                </li>
-            </ul>
-        </div>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "json_practice") or die('Connection Error');
+$sql = "SELECT * FROM `student_info`";
+$result = mysqli_query($conn, $sql) or die('Query Error');
+/*$data_arr = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo "<pre>";
+echo json_encode($data_arr);
+echo "</pre>";*/
+
+$output = array();
+while ($row = mysqli_fetch_assoc($result)) {
+    $output[] = $row;
+
+}
+echo "<pre>";
+echo json_encode($output);
+echo "</pre>";
+
+
+
+mysqli_close($conn);
