290b615ddd5376ea73e84c538bb6b9b423e4585a
Rasel Sheikh <raselsheikh123@gmail.com>
2025-02-12 08:53:56 +0600

    mysql connect and json encoded

--- config.php
+++ config.php
@@ -4,12 +4,6 @@ $sql = "SELECT * FROM `student_info`";
 $result = mysqli_query($conn, $sql) or die('Query Error');
 $data_arr = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-/*
-$data_arr = array();
-while ($row = mysqli_fetch_assoc($result)) {
-    $data_arr[] = $row;
-
-}*/
 echo "<pre>";
 echo json_encode($data_arr);
 echo "</pre>";
